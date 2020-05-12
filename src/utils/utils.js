52a34c53e5b1a35aecbf99f962f0b5991a642464
Marwanrizik91 <marwan.rizik@gmail.com>
2020-05-12 14:17:09 +0300

    initial files setup

---
+++ src/utils/utils.js
@@ -0,0 +1,9 @@
+export function getPokemon(name) {
+    return window
+      .fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      .then((res) => {
+        if (!res.ok) throw new Error("HTTP error");
+        return res;
+      })
+      .then((res) => res.json());
+  }
