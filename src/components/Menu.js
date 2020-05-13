f0e3e24d8447e6c2f489a04c1aa8d44b69ec9633
Jake <jakepowis@gmail.com>
2020-05-14 01:22:44 +0300

    reset game state fix

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -129,7 +129,7 @@ export function Attack({ player, setPlayer, enemy, setEnemy, gameState, setGameS
                 <div className="right-menu">
                     <h3 onClick={playerTurn ? playerAttack : enemyAttack}>{menu ? "ATTACK" : "DEFEND"}</h3>
                     <h3 className={spr === "used" ? "used" : ""} onClick={playerTurn ? superAttack : enemyAttack}>{menu ? "SUPER ATTACK" : "-"} </h3>
-                    <h3 className={mute === "false" ? "" : "used"} onClick={toggleMute}>{mute ? "MUTE" : "MUTED"}</h3>
+                    <h3 className={mute ? "" : "used"} onClick={toggleMute}>{mute ? "MUTE" : "MUTED"}</h3>
                     <h3 onClick={() => {
                         setGameState({ ...gameState, gameState: "start" })
                         setPlayer({ ...player, hp: 100 })
@@ -139,7 +139,7 @@ export function Attack({ player, setPlayer, enemy, setEnemy, gameState, setGameS
                     }}> RUN</h3>
                 </div>
             </div>
-        </div >
+        </div>
     )
 }
 
